0a370e92c05851ea93851ed28fa801a2cd502a73
Luis Borbolla <administracion@borbolla-automation.com>
2017-05-27 21:42:05 -0500

    prueba pdf

--- borbolla_automation/sistema/printing/printing.py
+++ borbolla_automation/sistema/printing/printing.py
@@ -3,6 +3,32 @@ from reportlab.platypus import SimpleDocTemplate, Paragraph
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 from reportlab.lib.enums import TA_CENTER
 from django.contrib.auth.models import User 
+from reportlab.pdfgen import canvas
+from reportlab.lib.units import mm
+ 
+class NumberedCanvas(canvas.Canvas):
+    def __init__(self, *args, **kwargs):
+        canvas.Canvas.__init__(self, *args, **kwargs)
+        self._saved_page_states = []
+ 
+    def showPage(self):
+        self._saved_page_states.append(dict(self.__dict__))
+        self._startPage()
+ 
+    def save(self):
+        """add page info to each page (page x of y)"""
+        num_pages = len(self._saved_page_states)
+        for state in self._saved_page_states:
+            self.__dict__.update(state)
+            self.draw_page_number(num_pages)
+            canvas.Canvas.showPage(self)
+        canvas.Canvas.save(self)
+ 
+    def draw_page_number(self, page_count):
+        # Change the position of this to wherever you want the page number to be
+        self.drawRightString(211 * mm, 15 * mm + (0.2 * inch),
+                             "Page %d of %d" % (self._pageNumber, page_count))
+
 
 class MyPrint:
     def __init__(self, buffer, pagesize):
@@ -35,7 +61,7 @@ class MyPrint:
  
         # Release the canvas
         canvas.restoreState()
-    
+
     def print_users(self):
         buffer = self.buffer
         doc = SimpleDocTemplate(buffer,
