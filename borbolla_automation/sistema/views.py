2cde2d0740020caf1281ac944bed760584400089
Luis Borbolla <administracion@borbolla-automation.com>
2017-05-27 20:49:53 -0500

    prueba pdf

--- borbolla_automation/sistema/views.py
+++ borbolla_automation/sistema/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from sistema.printing.printing import MyPrint
 from io import BytesIO
  
-@staff_member_required
+#@staff_member_required
 def print_users(request):
     # Create the HttpResponse object with the appropriate PDF headers.
     response = HttpResponse(content_type='application/pdf')
