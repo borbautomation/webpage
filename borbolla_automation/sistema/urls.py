d5daa28d80dd30f1c508fc3c3f681fbb356ba49a
Luis Borbolla <administracion@borbolla-automation.com>
2017-05-27 20:36:34 -0500

    prueba pdf

---
+++ borbolla_automation/sistema/urls.py
@@ -0,0 +1,13 @@
+from sistema import views
+from sistema.printing import printing
+from django.conf.urls import url
+from django.contrib import admin
+from django.conf.urls import include
+
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^admin/', admin.site.urls),
+    url(r'^print/', admin.site.urls),
+       
+]
