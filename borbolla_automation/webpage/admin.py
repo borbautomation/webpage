1c2015bd7c6227c35ea83be4c7ed8fe870af5489
Luis Borbolla <administracion@borbolla-automation.com>
2017-05-29 14:10:08 -0500

    agregado base de datos formulario

--- borbolla_automation/webpage/admin.py
+++ borbolla_automation/webpage/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from webpage.models import *
 
+
+class FormularioContactoAdmin(admin.ModelAdmin):
+    list_display = ('nombre' , 'compania' , 'email',)
+    search_fields = ('nombre' , 'compania' , 'email',)
+
+
+
+admin.site.register(FormularioContacto,FormularioContactoAdmin)
 # Register your models here.
