3578860109631820283063961d0f83815451d609
Luis Borbolla <administracion@borbolla-automation.com>
2017-05-29 18:44:46 -0500

    mandar correo

--- borbolla_automation/webpage/views.py
+++ borbolla_automation/webpage/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponse
 #Add this import at the top of the file
 from webpage.forms import WebpageForm
 from django.http import HttpResponseRedirect
-
+from django.contrib.auth.models import User 
+from webpage.send_email.send_mail import send_email_base as contact_email
 
     
 def index(request):
@@ -14,6 +15,18 @@ def index(request):
 
     	if form.is_valid():
     		form.save(commit=True)
+    		users = User.objects.all()
+    		emails = []
+    		for user in users:
+    		    emails.append(user.email)
+            
+            text = 'Nombre : %s\nCompania : %s\nEmail : %s\n Mensaje : %s\n'%(request.POST['nombre'],
+            																  request.POST['compania'],
+            																  request.POST['email'],
+            																  request.POST['text'],)
+
+            _send_mail = contact_email(emails,'Nuevo mensaje desde pagina web!',text)
+
     		return HttpResponseRedirect('/gracias/')
 
     else:
