3a9e94e8672e7dda948a6e004c4028a435c2e10d
Luis Borbolla <administracion@borbolla-automation.com>
2017-05-29 18:09:46 -0500

    credenciales send_mail

---
+++ borbolla_automation/webpage/send_email/send_mail.py
@@ -0,0 +1,21 @@
+import smtplib
+from credentials import e_mail
+gmail_user = e_mail['user']
+gmail_password = e_mail['password']
+
+sent_from = gmail_user  
+to = ['administracion@borbolla-automation.com', 'lic.myriamdelgado@gmail.com']  
+subject = 'OMG Super Important Message'  
+body = 'Hey, whats up?\n\n- You'
+
+email_text = """Subject : {} \n\n {}""".format(subject,body)
+try:  
+    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    server.ehlo()
+    server.login(gmail_user, gmail_password)
+    server.sendmail(sent_from, to, email_text)
+    server.close()
+
+    print 'Email sent!'
+except:  
+    print 'Something went wrong...'
