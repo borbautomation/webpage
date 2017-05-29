57bcc6b2b847333285b1083279eb0b8928a7d155
Luis Borbolla <administracion@borbolla-automation.com>
2017-05-29 16:13:23 -0500

    prueba form 2

--- borbolla_automation/webpage/views.py
+++ borbolla_automation/webpage/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+#Add this import at the top of the file
+from webpage.forms import WebpageForm
+
+
     
 def index(request):
-    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    
+    form = WebpageForm()
+    context_dict = {'form': form}
+    if request.method == 'POST':
+    	form = WebpageForm(request.POST)
+
+    	if form.is_valid():
+    		form.save(commit=True)
+    		return index(request)
+
+    	else:
+    		print(form.errors)	
 
     return render(request, 'webpage/index.html', context=context_dict)
 # Create your views here.
