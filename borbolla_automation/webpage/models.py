fe194271cbbc543fe956f75c5c257f6c708472d7
Luis Borbolla <administracion@borbolla-automation.com>
2017-05-29 14:04:26 -0500

    agregado base de datos formulario

--- borbolla_automation/webpage/models.py
+++ borbolla_automation/webpage/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+class FormularioContacto(models.Model):
+    nombre = models.CharField(max_length = 128 , )
+    compania = models.CharField(max_length = 30)
+    email = models.EmailField(max_length = 50 )
+    text = models.TextField()
+
+    def __str__(self):
+        return '%s %s %s'%(self.nombre , self.compania , self.email)  
+
+
 # Create your models here.
