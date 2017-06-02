8bc9d603f422edacc773a2286094540a1ff6cf13
Luis Borbolla <administracion@borbolla-automation.com>
2017-06-02 11:37:40 -0500

    dolar bases de datos

--- borbolla_automation/dollar/admin.py
+++ borbolla_automation/dollar/admin.py
@@ -7,10 +7,14 @@ class BancoAdmin(admin.ModelAdmin):
     search_fields = ('nombre' , 'fecha_creacion')
 
 class PrecioAdmin(admin.ModelAdmin):
-    list_display = ('banco__nombre' , 'compra','venta')
+    list_display = ('get_nombre' , 'compra','venta')
     search_fields = ('banco__nombre' , 'fecha_creacion')
+    def get_nombre(self,obj):
+        return obj.banco.nombre
+
+    get_nombre.admin_order_field  = 'nombre'  #Allows column order sorting
+    get_nombre.short_description = 'Cliente de Contacto'  #Renames column head  
 
 admin.site.register(Banco,BancoAdmin)
 admin.site.register(Precio,PrecioAdmin)    
 
-# Register your models here.
