2433406d8e096311eff3f777b9f8e06dc2f16a0c
Borbolla Automation <administracion@borbolla-automation.com>
2020-05-06 11:15:57 -0500

    tenplate de nueva pagina web

---
+++ borbolla_automation/static/industrial/inc/js.inc.php
@@ -0,0 +1,20 @@
+<script src="plugins/jquery.min.js"></script>
+<script src="plugins/bootstrap/js/bootstrap.min.js"></script>
+<script src="plugins/owl.carousel-2/owl.carousel.min.js"></script>
+
+<script src="https://maps.googleapis.com/maps/api/js"></script>
+<script src="http://cdnjs.cloudflare.com/ajax/libs/gmaps.js/0.4.24/gmaps.min.js"></script>
+<script src="plugins/map-helper.js"></script>
+<script src="plugins/jquery.mixitup.min.js"></script>
+<script src="plugins/bootstrap-select/dist/js/bootstrap-select.min.js"></script>
+
+<script src="plugins/Chart.min.js"></script>
+<script src="plugins/jquery-validation/dist/jquery.validate.min.js"></script>
+
+<script src="plugins/waypoints.min.js"></script>
+<script src="plugins/jquery.counterup.min.js"></script>
+
+<script src="js/theme.js"></script>
+
+
+<script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
---
+++ borbolla_automation/static/industrial/inc/style.inc.php
@@ -0,0 +1,14 @@
+<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,800,800italic,700italic' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Raleway:400,100,100italic,200,200italic,300,300italic,400italic,500,500italic,600,600italic,700,700italic,800,800italic,900' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=PT+Serif:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
+
+
+<link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" href="plugins/fontawesome/css/font-awesome.min.css">
+<link rel="stylesheet" href="plugins/fncp-icon/style.css">
+<link rel="stylesheet" href="plugins/Stroke-Gap-Icons-Webfont/style.css">
+<link rel="stylesheet" href="plugins/animate.min.css">
+<link rel="stylesheet" href="plugins/owl.carousel-2/assets/owl.carousel.css">
+<link rel="stylesheet" href="plugins/owl.carousel-2/assets/owl.theme.default.min.css">
+<link rel="stylesheet" href="plugins/bootstrap-select/dist/css/bootstrap-select.min.css">
